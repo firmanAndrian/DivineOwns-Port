2e5e89e8813ae81a57f32905c32ecece3044fd75
Firman Andriansyah <namrif52@gmail.com>
2023-08-30 19:39:05 +0700

    add section in home page

--- src/components/Fragments/Navbar.jsx
+++ src/components/Fragments/Navbar.jsx
@@ -1,15 +1,15 @@
 const Navbar =()=>{
 return(
     <div className="w-full h-[80px]  flex items-center fixed">
-            <div className="flex  h-full items-center w-full mx-[15px] sm:mx-[30px] lg:mx-[50px] justify-between">
-                <div className=" flex-auto basis-1/4 flex items-center text-zinc-100 text-[20px] sm:text-[22px] lg:text-[36px] font-black capitalize"><h1>DIVINE OWNS</h1></div>
-                <div className=" basis-4/6  flex flex-auto items-center justify-end hidden sm:inline-flex">
+            <div className="flex h-full items-center w-full mx-[15px] sm:mx-[30px] lg:mx-[50px] justify-between">
+                <div className=" flex-auto basis-1/4 flex items-center text-zinc-100 text-[20px] sm:text-[22px] md:text-[30px] lg:text-[36px] font-black capitalize"><h1>DIVINE OWNS</h1></div>
+                <div className=" basis-4/6  flex  items-center justify-end hidden sm:inline-flex">
                     <ul  className="flex">
-                        <li className="sm:text-[17px] md:text-[20px] lg:text-[25px] hed">About</li>
-                        <li className="sm:ml-6 sm:text-[17px] md:text-[20px] lg:text-[25px] hed">Product</li>
-                        <li className="sm:ml-6 sm:text-[17px] md:text-[20px] lg:text-[25px] hed">Tourney</li>
-                        <li className="sm:ml-3 sm:text-[17px] md:text-[20px] lg:text-[25px] border-[1px] border-[#A1A1A1]"></li>
-                        <li className="sm:ml-3 sm:text-[17px] md:text-[20px] lg:text-[25px] hed">Sign In</li>
+                        <li className="sm:text-[17px] md:text-[20px] lg:text-[29px] hed">About</li>
+                        <li className="sm:ml-6 lg:ml-10 sm:text-[17px] md:text-[20px] lg:text-[29px] hed">Product</li>
+                        <li className="sm:ml-6 lg:ml-10 sm:text-[17px] md:text-[20px] lg:text-[29px] hed">Tourney</li>
+                        <li className="sm:ml-3 lg:ml-5 sm:text-[17px] md:text-[20px] lg:text-[29px] border-[1px] border-[#A1A1A1]"></li>
+                        <li className="sm:ml-3 lg:ml-5 sm:text-[17px] md:text-[20px] lg:text-[29px] hed">Sign In</li>
                     </ul>
                 </div>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,15 +6,25 @@ const Home =() => {
     return(
         <AuthLayouts>
             <div className="max-w-full h-full">
-                <div className="max-w-full md:h-[1024px] layout h-screen flex  flex-col justify-center  bg-cover bg-center">
-                        <div className="flex flex-col w-[96%] -mt-10 sm:w-max ml-[9px] sm:ml-[40px] lg:ml-[52px] h-min sm:h-min ">
-                            <h2 className="w-[160px] sm:w-[200px] h-max md:w-[555px] sm:text-[28px] md:text-[32px] lg:text-[35px] begining">Get ready</h2>
-                            <div className="w-[98%] sm:w-[350px] md:w-[555px] sm:text-[24px] md:text-[28px] lg:text-[30px] pl-[20px] mt-[30px] sm:mt-[35px] md:mt-[40px] h-max begining-home">
+                <div className="max-w-full  layout h-[568px] sm:h-screen md:h-full flex  flex-col  bg-cover bg-center">
+                    <div className=" flex flex-col h-full justify-end sm:justify-between">
+                        <div className="flex flex-col w-[96%] sm:w-min ml-[9px] sm:ml-[33px] lg:ml-[52px] h-max mt-32 mb-14 sm:mt-52 md:mt-56 lg:mt-[193px] md:mb-[178px] lg:mb-[168px]">
+                            <h2 className="w-[160px] sm:w-[200px] h-max md:w-[555px] sm:text-[28px] md:text-[35px] lg:text-[47px] begining-home sm:text-[#fff]">Get Ready</h2>
+                            <div className="w-[98%] sm:w-[380px] md:w-[565px] sm:text-[24px] md:text-[28px] lg:text-[32px] pl-[20px] mt-[27px] sm:mt-[35px] md:mt-[50px] lg:mt-[46px] lg:pl-[44px] h-max begining-home">
                                 <h3>For an adrenaline-pumping gaming experience like never before!</h3>
-                                <h3 className="mt-[25px]">Step into the battleground and unleash your competitive spirit in our spectacular Tournament Game!</h3>
+                                <h3 className="mt-[27px] sm:mt-[35px] md:mt-[50px] lg:mt-[45px] ">Step into the battleground and unleash your competitive spirit in our spectacular Tournament Game!</h3>
                             </div>
                         </div>
-                        <div>halloooooo</div>
+                        <div className="flex flex-row flex-wrap ">
+                            <h3 className="basis-6/12 sm:basis-4/12 lg:basis-3/12 p-4 sm:p-7  bnnr bg-[#474747b2] text-[#FFF] sm:text-[23px] md:text-[28px] lg:text-[32px]" >Test your gaming prowess against the best of the best!</h3>
+                            <h3 className="basis-6/12 sm:basis-4/12 lg:basis-3/12 p-4 sm:p-7  bnnr bg-[#D8D6D6b2] text-[#000] sm:text-[23px] md:text-[28px] lg:text-[32px]">Face off with skilled opponents and prove that you have what it takes to claim victory.</h3>
+                            <div className="basis-full sm:basis-4/12 lg:basis-6/12 h-[90px] md:h-[310px] lg:h-[330px] sm:h-full bnnr bg-[#474747b2] flex flex-col items-center justify-between p-2 sm:p-7 sm:text-[23px] md:text-[28px] lg:text-[32px]">
+                                <h3 className="text-[#FFF] ">why wait ?</h3>
+                                
+                                <h3 className="text-[#FFF] text-[12px] w-max decro duration-500 hover:text-[12px] sm:text-[23px]  sm:hover:text-[23.5px] md:text-[28px] md:hover:text-[29.5px] lg:text-[32px] lg:hover:text-[33.5px]">Join now !</h3>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div></div>
             </div>
